8b2713c537e0444472162825e048541568d6c20c
Neurotechofficial <onyariallamano@gmail.com>
2026-04-15 00:56:21 +0300

    Create neko.js

---
+++ commands/neko.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+
+module.exports = async (sock, chatId, message) => {
+    try {
+        const res = await axios.get('https://api.giftedtech.co.ke/api/anime/neko?apikey=gifted');
+
+        const data = res.data.result || res.data;
+
+        await sock.sendMessage(chatId, {
+            image: { url: data },
+            caption: "🐱 *Neko Anime*"
+        }, { quoted: message });
+
+    } catch (error) {
+        console.log(error);
+        await sock.sendMessage(chatId, {
+            text: "❌ Failed to fetch neko image."
+        }, { quoted: message });
+    }
+};
