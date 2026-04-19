5699ead7ec7afc5d172bf5e6356becae6c46a559
Neurotechofficial <onyariallamano@gmail.com>
2026-04-19 04:53:11 +0300

    Update textmaker.js

--- commands/textmaker.js
+++ commands/textmaker.js
@@ -1,6 +1,17 @@
 const axios = require('axios');
 const mumaker = require('mumaker');
 
+// Base channel info template
+const channelInfo = {
+    forwardingScore: 1,
+    isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+        newsletterJid: '120363421451364996@newsletter',
+        newsletterName: 'Neurotech Official',
+        serverMessageId: -1
+    }
+};
+
 // Reusable message templates
 const messageTemplates = {
     error: (message) => ({
