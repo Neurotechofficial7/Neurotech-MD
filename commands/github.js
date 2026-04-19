aa18c690c206146ea2e6b1c5d7b6c2f58ebe6136
Neurotechofficial <onyariallamano@gmail.com>
2026-04-19 05:52:39 +0300

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -10,7 +10,7 @@ async function githubCommand(sock, chatId, message) {
     if (!res.ok) throw new Error('Error fetching repository data');
     const json = await res.json();
 
-    let txt = `*✦═════  ❖*Neurotech MD*❖ ═════✦ *\n\n`;
+    let txt = `✦═════❖Neurotech MD❖═══✦ \n\n`;
     txt += `❯  Name : ${json.name}\n`;
     txt += `❯  Watchers : ${json.watchers_count}\n`;
     txt += `❯  Size : ${(json.size / 1024).toFixed(2)} MB\n`;
@@ -19,6 +19,7 @@ async function githubCommand(sock, chatId, message) {
     txt += `❯  Forks : ${json.forks_count}\n`;
     txt += `❯  Stars : ${json.stargazers_count}\n\n`;
     txt += `💥 *Neurotech MD*`;
+    `✦═══════════════════════✦`
     
 
     // Use the local asset image
